57221bb958ac3a8874045f589f6243a72682ed10
KxllSwxtch <dimashin9@gmail.com>
2022-10-13 17:29:54 -0400

    started spootify

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,27 +1,48 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock"
+import "../styles/_discover.scss"
 
-export default class Discover extends Component {
-  constructor() {
-    super();
+const baseUrl = process.env.REACT_APP_SPOTIFY_BASE_URL
+const clientID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
 
-    this.state = {
-      newReleases: [],
-      playlists: [],
-      categories: []
-    };
-  }
+// For authorization purposes
+const authEndpoint = process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT
+const redirectURI = process.env.REACT_APP_SPOTIFY_REDIRECT_URI
+const responseType = process.env.REACT_APP_SPOTIFY_RESPONSE_TYPE
 
-  render() {
-    const { newReleases, playlists, categories } = this.state;
+const authURI = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}&show_dialog=true`
 
-    return (
-      <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
-      </div>
-    );
-  }
+const Discover = () => {
+	const [newReleases, setNewReleases] = useState([])
+	const [playlists, setPlaylists] = useState([])
+	const [categories, setCategories] = useState([])
+
+	useEffect(() => {
+		const getNewReleases = async () => {
+			const response = await axios.get(`${baseUrl}/browse/new-releases`)
+			console.log(response.data)
+		}
+	}, [])
+
+	return (
+		<div className="discover">
+			<a href={authURI}>Login to Spotify</a>
+			<DiscoverBlock
+				text="RELEASED THIS WEEK"
+				id="released"
+				data={newReleases}
+			/>
+			<DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
+			<DiscoverBlock
+				text="BROWSE"
+				id="browse"
+				data={categories}
+				imagesKey="icons"
+			/>
+		</div>
+	)
 }
+
+export default Discover
