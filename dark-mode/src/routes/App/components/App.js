88d148b508fdd9d7673bbfb4635ff4490ea785f2
KxllSwxtch <dimashin9@gmail.com>
2022-10-13 10:33:50 -0400

    dark-mode solved

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,52 +1,86 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import '../styles/_app.scss';
+import { useState, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
+import "../styles/_app.scss"
 
-function App() {
-  return (
-    <div className="app">
-      <div className="level">
-        <div>
-          <h1 className="title">Dark Mode Challenge</h1>
-        </div>
+const App = () => {
+	const savedMode = localStorage.getItem("savedMode")
+	const [currentMode, setCurrentMode] = useState(savedMode)
 
-        {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
-        </button>
+	const controlMode = () => {
+		if (currentMode === "dark") {
+			setCurrentMode("light")
+			localStorage.setItem("savedMode", "light")
+		} else {
+			setCurrentMode("dark")
+			localStorage.setItem("savedMode", "dark")
+		}
+	}
 
-      </div>
+	const isDarkMode = currentMode === "dark"
 
-      <div className="columns">
-        <div className="column">
-          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
-        </div>
-        <div className="column">
-          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
-        </div>
-      </div>
+	return (
+		<div className={isDarkMode ? "app dark-mode" : "app"}>
+			<div className="level">
+				<div>
+					<h1 className="title">Dark Mode Challenge</h1>
+				</div>
 
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Name" />
-        </div>
-      </div>
+				{/* --The button that should toggle dark mode-- */}
+				<button
+					className="app__dark-mode-btn icon level-right"
+					onClick={controlMode}
+				>
+					{isDarkMode ? (
+						<FontAwesomeIcon color="white" icon={faSun} />
+					) : (
+						<FontAwesomeIcon icon={faMoon} />
+					)}
+				</button>
+			</div>
 
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Email" />
-        </div>
-      </div>
+			<div className="columns">
+				<div className="column">
+					<p>
+						Lollipop powder powder. Cotton candy caramels chupa chups halvah
+						muffin caramels apple pie topping cake. Topping chocolate bar pastry
+						chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+						plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+						pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+						tiramisu chocolate bar carrot cake lemon drops halvah.
+					</p>
+				</div>
+				<div className="column">
+					<p>
+						Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+						tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+						marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+						Pudding sugar plum chocolate cake cake biscuit pastry pastry
+						chocolate bar tart. Lemon drops dessert gummies icing.
+					</p>
+				</div>
+			</div>
 
-      <section className="section">
-        <div className="buttons level-right">
-          <a className="button is-primary">Save</a>
-          <a className="button is-link">Submit</a>
-        </div>
-      </section>
-    </div>
-  );
+			<div className="field">
+				<div className="control">
+					<input className="input" type="text" placeholder="Name" />
+				</div>
+			</div>
+
+			<div className="field">
+				<div className="control">
+					<input className="input" type="text" placeholder="Email" />
+				</div>
+			</div>
+
+			<section className="section">
+				<div className="buttons level-right">
+					<a className="button is-primary">Save</a>
+					<a className="button is-link">Submit</a>
+				</div>
+			</section>
+		</div>
+	)
 }
 
-export default App;
+export default App
