c6dd8672542126dd9a5344b2ec71abe43ebf639f
KxllSwxtch <dimashin9@gmail.com>
2022-10-14 11:16:04 -0400

    started chatter

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,32 +1,40 @@
-import React, { useContext } from 'react';
-import io from 'socket.io-client';
-import useSound from 'use-sound';
-import config from '../../../config';
-import LatestMessagesContext from '../../../contexts/LatestMessages/LatestMessages';
-import TypingMessage from './TypingMessage';
-import Header from './Header';
-import Footer from './Footer';
-import Message from './Message';
-import '../styles/_messages.scss';
+import React, { useContext, useState } from "react"
+import io from "socket.io-client"
+import useSound from "use-sound"
+import config from "../../../config"
+import LatestMessagesContext from "../../../contexts/LatestMessages/LatestMessages"
+import TypingMessage from "./TypingMessage"
+import Header from "./Header"
+import Footer from "./Footer"
+import Message from "./Message"
+import "../styles/_messages.scss"
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
+const socket = io(config.BOT_SERVER_ENDPOINT, {
+	transports: ["websocket", "polling", "flashsocket"],
+})
 
 function Messages() {
-  const [playSend] = useSound(config.SEND_AUDIO_URL);
-  const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
-  const { setLatestMessage } = useContext(LatestMessagesContext);
+	const [playSend] = useSound(config.SEND_AUDIO_URL)
+	const [playReceive] = useSound(config.RECEIVE_AUDIO_URL)
+	const { setLatestMessage } = useContext(LatestMessagesContext)
 
-  return (
-    <div className="messages">
-      <Header />
-      <div className="messages__list" id="message-list">
-      </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
-    </div>
-  );
+	const [message, setMessage] = useState("")
+
+	const sendMessage = () => {}
+
+	const onChangeMessage = (event) => setMessage(event.target.value)
+
+	return (
+		<div className="messages">
+			<Header />
+			<div className="messages__list" id="message-list"></div>
+			<Footer
+				message={message}
+				sendMessage={sendMessage}
+				onChangeMessage={onChangeMessage}
+			/>
+		</div>
+	)
 }
 
-export default Messages;
+export default Messages
